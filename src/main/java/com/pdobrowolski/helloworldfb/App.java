5131ece955b0c105519b79e20a3d49290d21e1a2
Piotr Dobrowolski <piotrekdobroolski87@gmail.com>
2018-09-11 17:46:31 +0200

    First commit

---
+++ src/main/java/com/pdobrowolski/helloworldfb/App.java
@@ -0,0 +1,10 @@
+package com.pdobrowolski.helloworldfb;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
